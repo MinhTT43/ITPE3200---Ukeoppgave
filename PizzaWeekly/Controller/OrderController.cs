5bb34f4078bcb75f78eef89f5244eda5d9dc2e1b
minh <minhtt43@gmail.com>
2021-08-10 16:26:14 +0200

    Initial check-in of module PizzaWeekly

---
+++ PizzaWeekly/Controller/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PizzaWeekly.Model;
+
+namespace PizzaWeekly.Controller
+{
+    [Route("[controller]/[action]")]
+    public class OrderController : ControllerBase
+    {
+        public List<Order> GetOrders()
+        {
+           var OrderList = new List<Order>();
+
+            var order1 = new Order {
+                Type = "Margarita",
+                Crust = "American",
+                Quantity = 3,
+                Name = "Vivi Lu",
+                Address = "Lørenvangen 1",
+                Phone = "12345678"
+            };             
+
+            OrderList.Add(order1);
+
+            return OrderList;
+        }
+    }
+}
---
+++ PizzaWeekly/Model/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PizzaWeekly.Model
+{
+    public class Order
+    {
+        public int OId { get; set; }
+        public string Type { get; set; }
+        public string Crust { get; set; }
+        public int Quantity { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
